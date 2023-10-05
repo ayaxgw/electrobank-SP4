d6d25f705b618042ab07b145e48ea1d26656ec2f
Ayax Garcia Williner <agarciawilliner@gmail.com>
2023-10-05 20:10:17 -0300

    Cambiando nombre de csv y agregando filtro fechas

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,45 +1,57 @@
 import csv
 import sys
 import datetime
-#No hicimos uso del argparse
 
-print(sys.argv)
-#DATOS OBLIGATORIOS QUE DEBE ENVIAR EL USUARIO
+# Validar que se proporcionen suficientes argumentos
+if len(sys.argv) < 5:
+    print("Uso: python script.py <filename> <dni_a_filtrar> <pantalla_o_csv> <tipo> [estado] [fecha_a_filtrar]")
+    sys.exit(1)
+
+# Obtener argumentos de la línea de comandos
 filename = sys.argv[1]
 dni_a_filtrar = sys.argv[2]
 pantalla_o_csv = sys.argv[3]
 tipo = sys.argv[4]
-#OPCIONALES
-estado = None #Esto por lo que vi hace que no tire error out of range en el codigo
-fecha_a_filtrar = None #Esto por lo que vi hace que no tire error out of range en el codigo
+estado = None
+fecha_a_filtrar = None
 
+# Leer argumentos opcionales si están presentes
 if len(sys.argv) > 5:
-     estado = sys.argv[5]
+    estado = sys.argv[5]
 
 if len(sys.argv) > 6:
-     fecha_a_filtrar = sys.argv[6]     
+    fecha_a_filtrar = sys.argv[6]
 
-
+# Abre el archivo y lee el contenido
 with open(filename, "r") as file:
-        reader = csv.DictReader(file)
-        datos = [row for row in reader]
+    reader = csv.DictReader(file)
+    datos = [row for row in reader]
 
-datos_filtrados = [dato for dato in datos if dato ['DNI'] == dni_a_filtrar]
-datos_filtrados = [dato for dato in datos_filtrados if dato ['Tipo'] == tipo]
+# Filtrar los datos según los criterios especificados
+datos_filtrados = [dato for dato in datos if dato['DNI'] == dni_a_filtrar]
+datos_filtrados = [dato for dato in datos_filtrados if dato['Tipo'] == tipo]
 
 if estado is not None:
-    datos_filtrados = [dato for dato in datos_filtrados if dato ['Estado'] == estado]
+    datos_filtrados = [dato for dato in datos_filtrados if dato['Estado'] == estado]
 
-if pantalla_o_csv == 'CSV':  
-    #with open("salida.csv", "w") as salida:
-     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S") #Esto le da formato cuando se crea el .csv con fecha
-     with open(f"{dni_a_filtrar}_{timestamp}.csv","w") as salida:
+if fecha_a_filtrar is not None:
+    fecha_a_filtrar = datetime.datetime.strptime(fecha_a_filtrar, '%Y-%m-%d').date()
+    datos_filtrados = [dato for dato in datos_filtrados if datetime.datetime.strptime(dato['Fecha'], '%Y-%m-%d').date() == fecha_a_filtrar]
+
+# Parte que valida si se desea la salida en CSV o en pantalla
+if pantalla_o_csv == 'CSV':
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    output_filename = f"{dni_a_filtrar}_{timestamp}.csv"
+
+    with open(output_filename, "w") as salida:
         writer = csv.DictWriter(salida, fieldnames=reader.fieldnames)
         writer.writeheader()
         writer.writerows(datos_filtrados)
+    print(f"Datos filtrados guardados en {output_filename}")
+
 elif pantalla_o_csv == 'PANTALLA':
     for fila in datos_filtrados:
-        print(fila)
+        print(f"Fila: {fila}")
 else:
     print("Opción incorrecta. Elegir entre 'CSV' o 'PANTALLA'.")
-     
+
